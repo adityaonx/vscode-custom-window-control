4d525d60d4e345bd92f995e5959f416160f5ec59
Aditya <adityaonx@gmail.com>
2025-03-01 01:40:41 +0530

    11

--- script.js
+++ script.js
@@ -1,22 +1,34 @@
 /* eslint-env browser */
 (function () {
+  // change style
   const style = Object.freeze({
-    closeColor: "#ff5f56",
-    minimizeColor: "#ffbd2e",
-    maximizeColor: "#27c93f",
-    size: "8px", // Desired size
-    gap: "6px" // Adjust gap for better fit
+    // button background colors
+    closeColor: "#ff5f56", // red
+    minimizeColor: "#ffbd2e", // yellow
+    maximizeColor: "#27c93f", // green
+    // button size
+    size: "15px",
+    // border radius
+    radius: "50%",
+    // button hover background color
+    hoverBackgroundColor: "#33373e",
+    // button text color
+    color: "#c0c0c0",
+    fontSize: 10,
+    fontWeight: "700",
+    gap: "6px"
   });
 
   window.addEventListener("DOMContentLoaded", () => {
     let timer = setInterval(() => {
       const controls = document.querySelector(".titlebar-container .titlebar-right .window-controls-container");
       const leftTitlebar = document.querySelector(".titlebar-container .titlebar-left");
-
+      // not loaded
       if (!controls || !leftTitlebar) {
         return;
       }
 
+      // put close button in first position
       controls.parentNode.removeChild(controls);
       const closeControl = controls.lastChild;
       controls.removeChild(closeControl);
@@ -25,7 +37,12 @@
       controls.style.width = "auto";
       controls.style.gap = controls.style.marginLeft = style.gap;
       controls.childNodes.forEach((child, index) => {
-        child.classList.add("custom-control-button"); // Apply the CSS class
+        child.style.borderRadius = style.radius;
+        child.style.width = child.style.height = style.size;
+        child.style.margin = "auto";
+        child.style.cursor = "pointer";
+        child.style.fontWeight = style.fontWeight;
+        child.style.fontSize = `${style.fontSize}px`;
         child.style.backgroundColor =
           index === 0 ? style.closeColor : index === 1 ? style.minimizeColor : style.maximizeColor;
         child.addEventListener("mouseleave", () => child.style.backgroundColor =
@@ -35,11 +52,5 @@
       leftTitlebar.appendChild(controls);
       clearInterval(timer);
     }, 100);
-
-    // Hide favicon using CSS
-    const link = document.querySelector('link[rel="icon"]');
-    if (link) {
-      link.href = ''; // Removes the favicon by setting an empty URL
-    }
   });
 })();
