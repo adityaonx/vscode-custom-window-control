a9f62e926796d7a8d9910b80d3fe754c3338fb2e
Aditya <adityaonx@gmail.com>
2025-03-01 01:31:31 +0530

    11

--- script.js
+++ script.js
@@ -4,8 +4,13 @@
     closeColor: "#ff5f56",
     minimizeColor: "#ffbd2e",
     maximizeColor: "#27c93f",
-    size: "5px", // Desired size
-    gap: "6px" // Adjust gap for better fit
+    size: "13px", // Desired size
+    radius: "50%",
+    hoverBackgroundColor: "#33373e",
+    color: "#c0c0c0",
+    fontSize: "8px", // Adjust font size for better fit
+    fontWeight: "700",
+    gap: "10px"
   });
 
   window.addEventListener("DOMContentLoaded", () => {
@@ -25,7 +30,19 @@
       controls.style.width = "auto";
       controls.style.gap = controls.style.marginLeft = style.gap;
       controls.childNodes.forEach((child, index) => {
-        child.classList.add("custom-control-button"); // Apply the CSS class
+        child.style.borderRadius = style.radius;
+        child.style.width = style.size;
+        child.style.height = style.size;
+        child.style.minWidth = style.size;
+        child.style.minHeight = style.size;
+        child.style.margin = "auto";
+        child.style.cursor = "pointer";
+        child.style.fontWeight = style.fontWeight;
+        child.style.fontSize = style.fontSize;
+        child.style.display = "flex";
+        child.style.alignItems = "center";
+        child.style.justifyContent = "center";
+        child.style.boxSizing = "border-box";
         child.style.backgroundColor =
           index === 0 ? style.closeColor : index === 1 ? style.minimizeColor : style.maximizeColor;
         child.addEventListener("mouseleave", () => child.style.backgroundColor =
