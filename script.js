6acb5c60dc257e206ebd8836a3a28c82e672c25a
Aditya <adityaonx@gmail.com>
2025-03-01 01:13:44 +0530

    10

--- script.js
+++ script.js
@@ -4,7 +4,7 @@
     closeColor: "#ff5f56",
     minimizeColor: "#ffbd2e",
     maximizeColor: "#27c93f",
-    size: "14px",
+    size: "13px",
     radius: "50%",
     hoverBackgroundColor: "#33373e",
     color: "#c0c0c0",
